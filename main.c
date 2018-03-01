49421cb23996cac393ba1b82a63ae1a71170e76a
vitor-vfm <vitor.vfm@gmail.com>
2018-03-01 16:50:43 -0700

    time and output

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 #include <math.h>
 
 #include "Lab4_IO.h"
+#include "timer.h"
 
 #define EPSILON 0.00001
 #define DAMPING_FACTOR 0.85
@@ -15,6 +16,8 @@
 int main(int argc, char *argv[])
 {
     MPI_Init(&argc, &argv);
+
+    double start_time, end_time;
 
     int rank, comm_size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -33,6 +36,8 @@ int main(int argc, char *argv[])
     // Calculate the result
     if (node_init(&nodehead, num_in_links, num_out_links, 0, nodecount)) return 254;
 
+    GET_TIME(start_time);
+
     const unsigned chunk_size = nodecount / comm_size;
     const unsigned start = rank * chunk_size;
     const unsigned end = start + chunk_size <= nodecount ? start + chunk_size : nodecount;
@@ -45,7 +50,6 @@ int main(int argc, char *argv[])
 
     // CORE CALCULATION
     do{
-        printf("about to bcast from %u to %u, process %u, iter %d\n", start, end, rank, iterationcount);
         ++iterationcount;
         vec_cp(r, r_pre, nodecount);
 
@@ -61,26 +65,13 @@ int main(int argc, char *argv[])
         // send the chunk to other processes
         MPI_Allgather(&r[start], end-start, MPI_DOUBLE, r, end-start, MPI_DOUBLE, MPI_COMM_WORLD);
 
-        // read the chunks of other processes
-        /* for (unsigned proc = 0; proc < comm_size; proc++) { */
-        /*     if (proc == rank) { */
-        /*         printf("about to bcast from %u to %u, process %u", start, end, rank); */
-        /*         MPI_Bcast(&r[start], (end-start), MPI_DOUBLE, rank, MPI_COMM_WORLD); */
-        /*     } else { */
-        /*         unsigned this_start = proc * chunk_size; */
-        /*         unsigned this_end = this_start + chunk_size; */
-        /*         this_end = this_end <= nodecount ? end : nodecount; */
-
-        /*         MPI_Status status; */
-
-        /*         printf("about to recv from %u to %u, process %u", start, end, rank); */
-        /*         MPI_Recv(&r[this_start], (this_end-this_start), MPI_DOUBLE, */
-        /*                 proc, MPI_ANY_TAG, MPI_COMM_WORLD, &status); */
-        /*     } */
-        /* } */
-        
-
     } while(rel_error(r, r_pre, nodecount) >= EPSILON);
+
+    GET_TIME(end_time);
+
+    if (rank == 0) {
+        Lab4_saveoutput(r, nodecount, end_time - start_time);
+    }
 
     // post processing
     node_destroy(nodehead, nodecount);
